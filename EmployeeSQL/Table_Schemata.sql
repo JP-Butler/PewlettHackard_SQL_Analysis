336449b1cfbb1d0a7e8cb539c7d2e7d235ee3cbb
JP-Butler <justinpbutler12@gmail.com>
2023-06-16 23:01:17 -0400

    adding table schemata & erd image

---
+++ EmployeeSQL/Table_Schemata.sql
@@ -0,0 +1,74 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/HykK8h
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- PART 2: DATA ENGINEERING
+
+-- Table Creation Schemata
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR(7)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(7)   NOT NULL
+);
+
+CREATE TABLE "Dept_manager" (
+    "dept_no" VARCHAR(7)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title" VARCHAR(10)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(5)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "Employees" 
+RENAME COLUMN "emp_title" TO "emp_title_id";
+
+CREATE TABLE "Salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+-- Table alterations to establish foreign key restraint relationships to primary keys above
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+SELECT * FROM "Departments";
